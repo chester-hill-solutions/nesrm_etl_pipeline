448a34a3c571f2ca4506ed9f10396c6b86531bfd
Saihaan Syed <saihaansyed2003+macbookair@gmail.com>
2026-01-26 16:22:47 -0500

    change envs

--- loadEnv.js
+++ loadEnv.js
@@ -2,7 +2,7 @@
 import fs from "fs";
 import dotenv from "dotenv";
 
-const envFile = ".env"+process.env.ENVIRONMENT;
+const envFile = process.env.ENVIRONMENT ? ".env"+process.env.ENVIRONMENT : ".env.local" ?? ".env";
 
 if (fs.existsSync(envFile)) {
   dotenv.config({ path: envFile });
