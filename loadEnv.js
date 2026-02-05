a988b08c5bf7792501ede40463f615595835b0c2
Saihaan Syed <saihaansyed2003+macbookair@gmail.com>
2026-02-05 15:43:15 -0500

    changed loadenv

--- loadEnv.js
+++ loadEnv.js
@@ -2,11 +2,16 @@
 import fs from "fs";
 import dotenv from "dotenv";
 
-const envFile = process.env.ENVIRONMENT ? ".env"+"."+process.env.ENVIRONMENT : ".env" ?? ".env.local" ;
-
-if (fs.existsSync(envFile)) {
-  dotenv.config({ path: envFile });
-  console.log(`✅ Loaded ${envFile}`);
-} else {
-  console.warn(`⚠️ ${envFile} not found, using process defaults`);
+const envFile = process.env.ENVIRONMENT
+  ? ".env" + "." + process.env.ENVIRONMENT
+  : (".env" ?? ".env.local");
+const envFiles = [".env." + process.env.ENVIRONMENT, ".env", ".env.local"];
+for (let index = 0; index < envFiles.length; index++) {
+  if (fs.existsSync(envFiles[index])) {
+    dotenv.config({ path: envFiles[index] });
+    console.log(`✅ Loaded ${envFiles[index]}`);
+    break;
+  } else {
+    console.warn(`⚠️ ${envFiles} not found, using process defaults`);
+  }
 }
